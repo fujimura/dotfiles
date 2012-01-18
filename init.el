ac03afb1b9c8c58b7c433a6cdd23651ea4f77c1e
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-01-19 02:08:15 +0900

    more init.el

--- init.el
+++ init.el
@@ -2,7 +2,27 @@
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-ruby
+                      starter-kit-js
+                      starter-kit-eshell
+                      yaml-mode
+                      haml-mode)
+  "A list of packages to ensure are installed at launch.")
+(set-face-attribute 'default nil :height 130)
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 (setq-default tab-width 2)
@@ -11,6 +31,7 @@
 (setq linum-format "%4d " )
 (menu-bar-mode t)
 (global-linum-mode t)
+(setq-default show-trailing-whitespace t)
 
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
@@ -31,3 +52,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(transient-mark-mode t)
+
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
+(ac-config-default)
