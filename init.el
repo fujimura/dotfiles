f99111e5cad301ae2a7ddd09c537e51c4de0e857
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-01-18 20:04:40 +0900

    el

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
 (setq linum-format "%4d " )
-
+(menu-bar-mode t)
 (global-linum-mode t)
 
 (require 'yaml-mode)
@@ -20,7 +20,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t)))))
+ '(blink-cursor-mode nil)
+ '(custom-enabled-themes (quote (tango)))
+ '(safe-local-variable-values (quote ((encoding . utf-8) (whitespace-line-column . 80) (lexical-binding . t))))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
