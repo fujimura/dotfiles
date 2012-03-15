f333988f6c9387e85c78b220bf370bbeab9be748
Fujimura Daisuke <dfujimura@aiming-inc.com>
2012-03-16 00:10:09 +0900

    Reorganize init.el

--- init.el
+++ init.el
@@ -14,7 +14,6 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-;; Add in your own as you wish:
 (defvar my-packages '(starter-kit
                       starter-kit-lisp
                       starter-kit-ruby
@@ -29,14 +28,14 @@
                       markdown-mode
                       clojure-mode
                       git-commit
-                      ghc)
-"A list of packages to ensure are installed at launch.")
-(set-face-attribute 'default nil :height 140)
+                      ghc))
 
+;; Install packages unless installed
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
+(set-face-attribute 'default nil :height 140)
 (setq-default tab-always-indent t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -44,18 +43,19 @@
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-message t)
 (setq linum-format "%4d " )
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-
 (global-linum-mode t)
 (setq-default show-trailing-whitespace t)
-
-(require 'yaml-mode)
-(require 'coffee-mode)
-(require 'slim-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+(global-visual-line-mode 1) ; 1 for on, 0 for off.
+(setq line-spacing 3)
+(setq skeleton-pair 1)
+(delete-selection-mode t)
+(transient-mark-mode t)
+(require 'hl-line)
+(set-face-background 'hl-line "black")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -66,40 +66,24 @@
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(vc-follow-symlinks t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(transient-mark-mode t)
 
+;; auto-complete
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
 (ac-config-default)
 
+;; Parentheses Completion
 (global-set-key (kbd "(") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "{") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "[") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "\"") 'skeleton-pair-insert-maybe)
 (global-set-key (kbd "'") 'skeleton-pair-insert-maybe)
-(global-visual-line-mode 1) ; 1 for on, 0 for off.
-(setq line-spacing 3)
-(setq skeleton-pair 1)
+
+;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (setq ghc-module-command "~/Library/Haskell/bin/ghc-mod")
-(delete-selection-mode t)
 (autoload 'ghc-init "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda ()
                                (ghc-init)
                                (flymake-mode)))
-
-(if (window-system)
-    (progn (create-fontset-from-ascii-font "Monaco-14:weight=normal:slant=normal" nil "menlokakugo")
-           (set-fontset-font "fontset-menlokakugo"
-                             'unicode
-                             (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
-                             nil
-                             'append)
-           (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))))
-(require 'hl-line)
-(set-face-background 'hl-line "black")
