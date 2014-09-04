9b238ba4ee9ed56da9320d1330e53bb2831f3410
Fujimura Daisuke <me@fujimuradaisuke.com>
2014-09-04 19:14:34 +0900

    Rewrite repo in Ruby

--- .repo.zsh
+++ .repo.zsh
@@ -1,16 +1,3 @@
-function repo(){
-  if [ -z $1 ]; then
-    choice=$(ghq list --unique | peco)
-    if [ -z $choice ]; then
-      echo "No repo was chosen"
-    else
-      ghq look $choice
-    fi
-  else
-    ghq look $1
-  fi
-}
-
 function _repo {
   _values $(ghq list --unique)
 }
