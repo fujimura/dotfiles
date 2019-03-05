9d6aebb9262e1ec271d5946b255666b9dba17ef1
Daisuke Fujimura <me@fujimuradaisuke.com>
2019-03-05 18:11:41 +0900

    Add shell script to install dotfiles

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+set -xe
+
+ln -s "$PWD"/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s "$PWD"/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
