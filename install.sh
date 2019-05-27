1cc984828e7884003a82c9e185fb22b4592c0628
Daisuke Fujimura <me@fujimuradaisuke.com>
2019-05-27 18:55:35 +0900

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #! /bin/sh
 
-set -xe
+set -x
 
+mkdir -p ~/.bin
+ln -s "$PWD"/bin/git-bk ~/.bin/
+ln -s "$PWD"/bin/git-run ~/.bin/
+ln -s "$PWD"/bin/git-tb ~/.bin/
+ln -s "$PWD"/.gemrc ~/
+ln -s "$PWD"/.gitconfig ~/
+ln -s "$PWD"/.gitignore ~/
+ln -s "$PWD"/.prompt.zshrc ~/
+ln -s "$PWD"/.psqlrc ~/
+ln -s "$PWD"/.tigrc ~/
+ln -s "$PWD"/.tmux.conf ~/
+ln -s "$PWD"/.zshenv ~/
+ln -s "$PWD"/.prompt.zshrc ~/
 ln -s "$PWD"/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s "$PWD"/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
